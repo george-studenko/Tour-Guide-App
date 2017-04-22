48a2a145ef54ec5453c70de624085cd843130ac8
George Studenko <gnstudenko@gmail.com>
2017-04-22 22:41:49 +0200

    feat: Add SimpleFragmentPagerAdapter basic code

---
+++ app/src/main/java/com/georgestudenko/tourguide/Adapters/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,30 @@
+package com.georgestudenko.tourguide.Adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by george on 22/04/2017.
+ */
+
+public class SimpleFragmentPagerAdapter extends FragmentStatePagerAdapter{
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+    }
+}
