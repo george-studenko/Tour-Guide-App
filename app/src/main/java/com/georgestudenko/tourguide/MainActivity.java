9993994706ac4509539235336925e2a966cc3f81
George Studenko <gnstudenko@gmail.com>
2017-04-23 19:06:16 +0200

    refactor: Implement MainActivity onCreate ViewPager and TabLayout

--- app/src/main/java/com/georgestudenko/tourguide/MainActivity.java
+++ app/src/main/java/com/georgestudenko/tourguide/MainActivity.java
@@ -1,7 +1,17 @@
 package com.georgestudenko.tourguide;
 
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.georgestudenko.tourguide.Adapters.PlaceAdapter;
+import com.georgestudenko.tourguide.Adapters.SimpleFragmentPagerAdapter;
+import com.georgestudenko.tourguide.Models.Place;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+        TabLayout tabs = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabs.setupWithViewPager(viewPager);
+
     }
 }
