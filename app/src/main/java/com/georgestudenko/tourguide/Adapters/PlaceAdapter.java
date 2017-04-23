322ee582cb9c8680e9f0fba1b13666a7a1dafcaa
George Studenko <gnstudenko@gmail.com>
2017-04-23 16:21:52 +0200

    feat: Add PlaceAdapter constructor

--- app/src/main/java/com/georgestudenko/tourguide/Adapters/PlaceAdapter.java
+++ app/src/main/java/com/georgestudenko/tourguide/Adapters/PlaceAdapter.java
@@ -4,21 +4,30 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.georgestudenko.tourguide.Models.Place;
+import com.georgestudenko.tourguide.R;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.resource;
 
 /**
  * Created by george on 22/04/2017.
  */
 
 public class PlaceAdapter extends ArrayAdapter<Place> {
-    public PlaceAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<Place> objects) {
-        super(context, resource, objects);
+    private ArrayList<Place> mPlaces;
+    private Context mContext;
+    public PlaceAdapter(@NonNull Context context, ArrayList<Place> objects) {
+        super(context, 0, objects);
+        mPlaces=objects;
+        mContext=context;
     }
 
     @NonNull
