24ddca3c45928d6c71298189004629b854df2f3e
George Studenko <gnstudenko@gmail.com>
2017-04-23 19:02:27 +0200

    feat: Add PlacesListFragment onCreate to set the mData from args Bundle

--- app/src/main/java/com/georgestudenko/tourguide/PlacesListFragment.java
+++ app/src/main/java/com/georgestudenko/tourguide/PlacesListFragment.java
@@ -1,24 +1,37 @@
 package com.georgestudenko.tourguide;
 
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.georgestudenko.tourguide.Adapters.PlaceAdapter;
+import com.georgestudenko.tourguide.Models.Place;
 import com.georgestudenko.tourguide.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PlacesListFragment extends Fragment {
-
+    ArrayList<Place> mData;
 
     public PlacesListFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = this.getArguments();
+        mData =  bundle.getParcelableArrayList("data");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
