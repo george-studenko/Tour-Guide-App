ccb0bbacbcb715de4668cfb85b3e06278cbe337a
George Studenko <gnstudenko@gmail.com>
2017-04-23 18:45:56 +0200

    feat: Implement PlaceAdapter getView method

--- app/src/main/java/com/georgestudenko/tourguide/Adapters/PlaceAdapter.java
+++ app/src/main/java/com/georgestudenko/tourguide/Adapters/PlaceAdapter.java
@@ -1,21 +1,19 @@
 package com.georgestudenko.tourguide.Adapters;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.georgestudenko.tourguide.Models.Place;
 import com.georgestudenko.tourguide.R;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import static android.R.attr.resource;
 
 /**
  * Created by george on 22/04/2017.
@@ -24,6 +22,7 @@ import static android.R.attr.resource;
 public class PlaceAdapter extends ArrayAdapter<Place> {
     private ArrayList<Place> mPlaces;
     private Context mContext;
+
     public PlaceAdapter(@NonNull Context context, ArrayList<Place> objects) {
         super(context, 0, objects);
         mPlaces=objects;
@@ -33,8 +32,20 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+        View v = convertView;
+        if(v==null){
+            v = LayoutInflater.from(mContext).inflate(R.layout.place_item,parent,false);
+        }
+        Place place = mPlaces.get(position);
+
+        ImageView placeImage = (ImageView) v.findViewById(R.id.placeImage);
+        TextView placeName = (TextView) v.findViewById(R.id.placeName);
+        TextView placeShortDescription = (TextView) v.findViewById(R.id.placeShortDescription);
+
+        placeImage.setImageResource(place.getPictureResource());
+        placeName.setText(place.getName());
+        placeShortDescription.setText(place.getDescription());
+
+        return v;
     }
-
-
 }
